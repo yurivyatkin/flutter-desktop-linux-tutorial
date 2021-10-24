fef489bace6d9ffb6e8c9ff70c5069e3544b0603
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-10-24 22:59:41 +1300

    Include client credentials

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = '47168b0d400ce9c478f2';
+const githubClientSecret = 'c83464ee57a13970d2ee90b29df5641e12f6f610';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
