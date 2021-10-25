a8a463f00ee5338a650ff2e90febaac439fdd23f
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-10-25 14:10:26 +1300

    Put it all together and fix the typo I typed gcl instead of gql %)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:gql_exec/gql_exec.dart';
+import 'package:gql_http_link/gql_http_link.dart';
+import 'package:gql_link/gql_link.dart';
+
 import 'github_oauth_credentials.dart';
+import 'src/github_gql/github_queries.data.gql.dart';
+import 'src/github_gql/github_queries.req.gql.dart';
 import 'src/github_login.dart';
 
 void main() {
@@ -30,20 +36,55 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return GithubLoginWidget(
       builder: (context, httpClient) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text(title),
-          ),
-          body: const Center(
-            child: Text(
-              'You are logged in to GitHub!',
-            ),
-          ),
+        final link = HttpLink(
+          'https://api.github.com/graphql',
+          httpClient: httpClient,
+        );
+        return FutureBuilder<GViewerDetailData_viewer>(
+          future: viewerDetail(link),
+          builder: (context, snapshot) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text(title),
+              ),
+              body: Center(
+                child: Text(
+                  snapshot.hasData
+                      ? 'Hello ${snapshot.data!.login}!'
+                      : 'Retrieving viewer login details...',
+                ),
+              ),
+            );
+          },
         );
       },
       githubClientId: githubClientId,
       githubClientSecret: githubClientSecret,
       githubScopes: githubScopes,
     );
+  }
+}
+
+Future<GViewerDetailData_viewer> viewerDetail(Link link) async {
+  final req = GViewerDetail((b) => b);
+  final result = await link
+      .request(Request(
+        operation: req.operation,
+        variables: req.vars.toJson(),
+      ))
+      .first;
+  final errors = result.errors;
+  if (errors != null && errors.isNotEmpty) {
+    throw QueryException(errors);
+  }
+  return GViewerDetailData.fromJson(result.data!)!.viewer;
+}
+
+class QueryException implements Exception {
+  QueryException(this.errors);
+  List<GraphQLError> errors;
+  @override
+  String toString() {
+    return 'Query Exception: ${errors.map((err) => '$err').join(',')}';
   }
 }
