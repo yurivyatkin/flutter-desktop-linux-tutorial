7d1e5f6b408e6470b7cfd5eef37418596cc9c502
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-10-27 22:38:39 +1300

    Wire in the new widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:gql_exec/gql_exec.dart';
-import 'package:gql_link/gql_link.dart';
-import 'package:gql_http_link/gql_http_link.dart';
-import 'package:window_to_front/window_to_front.dart'; // Add this,
+import 'package:window_to_front/window_to_front.dart';
 
 import 'github_oauth_credentials.dart';
-import 'src/github_gql/github_queries.data.gql.dart';
-import 'src/github_gql/github_queries.req.gql.dart';
 import 'src/github_login.dart';
+import 'src/github_summary.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +20,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'GitHub GraphQL API Client'),
+      home: const MyHomePage(title: 'GitHub GraphQL API Client'),
     );
   }
 }
@@ -36,57 +32,18 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GithubLoginWidget(
-      builder: (context, httpClient) {
-        WindowToFront.activate(); // and this.
-        final link = HttpLink(
-          'https://api.github.com/graphql',
-          httpClient: httpClient,
-        );
-        return FutureBuilder<GViewerDetailData_viewer>(
-          future: viewerDetail(link),
-          builder: (context, snapshot) {
-            return Scaffold(
-              appBar: AppBar(
-                title: Text(title),
-              ),
-              body: Center(
-                child: Text(
-                  snapshot.hasData
-                      ? 'Hello ${snapshot.data!.login}!'
-                      : 'Retrieving viewer login details...',
-                ),
-              ),
-            );
-          },
+      builder: (context, client) {
+        WindowToFront.activate();
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+          ),
+          body: GitHubSummary(client: client),
         );
       },
       githubClientId: githubClientId,
       githubClientSecret: githubClientSecret,
       githubScopes: githubScopes,
     );
-  }
-}
-
-Future<GViewerDetailData_viewer> viewerDetail(Link link) async {
-  final req = GViewerDetail((b) => b);
-  final result = await link
-      .request(Request(
-        operation: req.operation,
-        variables: req.vars.toJson(),
-      ))
-      .first;
-  final errors = result.errors;
-  if (errors != null && errors.isNotEmpty) {
-    throw QueryException(errors);
-  }
-  return GViewerDetailData.fromJson(result.data!)!.viewer;
-}
-
-class QueryException implements Exception {
-  QueryException(this.errors);
-  List<GraphQLError> errors;
-  @override
-  String toString() {
-    return 'Query Exception: ${errors.map((err) => '$err').join(',')}';
   }
 }
